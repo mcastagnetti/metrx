14d3747574651d7fb31b6757f1c4a6c6b0236c5e
Marc-Olivier Castagnetti <6144489+mcastagnetti@users.noreply.github.com>
2021-11-03 09:52:03 +0100

    Fix wrong TB value

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -11,5 +11,5 @@ test('bytesToSize', async (t) => {
   t.is(bytesToSize(512), '512.00 Bytes');
   t.is(bytesToSize(2 * 1024 * 1024), '2.00 MB');
   t.is(bytesToSize(3 * 1024 * 1024 * 1024), '3.00 GB');
-  t.is(bytesToSize(4 * 1024 * 1024 * 1024 * 1024), '3.00 TB');
+  t.is(bytesToSize(4 * 1024 * 1024 * 1024 * 1024), '4.00 TB');
 });
